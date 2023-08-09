2f9ee2e9f4a48bee0f43554f26a7dcf5797a2365
Ningoje <dennismutuku474@gmail.com>
2023-08-09 15:56:17 +0300

    fix

---
+++ Home.php
@@ -0,0 +1,9 @@
+<div class="text-center col-md-12 border border-success">
+    <div class="wrapper">
+        <br>
+        <div class="typing-demo">
+
+            Hi friend👋👋, WELCOME!!!!!!
+        </div>
+    </div>
+</div>
