ecd9d78b0b72a29b67e5767f2b0abd588a4f24dd
Ningoje <dennismutuku474@gmail.com>
2023-09-01 16:49:03 +0300

    fix qr

--- qr.php
+++ qr.php
@@ -1,36 +1,19 @@
-<div class="row">
-
-
-  <div class="col-md-3"></div>
-  <div class="col-md-6">
-    <div class="container">
-      <h1>QR Code Generator</h1>
-      <div class="row m-3 -p3">
-        <div class="col-md-3">
-          <input type="text" id="searchTxt" class="form-control" placeholder="Enter text or link">
-        </div>
-        <div class="col-sm-2">
-          <button class="btn btn-primary" id="searchbtn">Generate Qr</button>
-
-        </div>
-        <div class="row m-3 -p-3">
-          <div class="col-md-3"></div>
-          <div class="col-md-6">
-            <div class="card">
-              <div class="card-body">
-                <div class="txtBox">
-
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-3"></div>
-        </div>
-      </div>
+<h2 class="page-header text-center">QR Code Generator</h2>
+<div class="card search-card m-2 p2">
+  <div class="row m-3 -p3">
+    <div class="col-md-3">
+      <input type="text" id="searchTxt" class="form-control" placeholder="Enter text or link">
+    </div>
+    <div class="col-sm-2">
+      <button class="btn btn-primary" id="searchbtn">Generate Qr</button>
 
     </div>
   </div>
-  <div class="col-md-3"></div>
+</div>
+<div class="txtBox">
+
+</div>
+<div class="col-md-3"></div>
 </div>
 
 
@@ -42,17 +25,34 @@
   document.querySelector("#searchbtn").addEventListener("click", qrGen);
 
   function qrGen(e) {
-    const name = document.querySelector("#searchTxt").value;
+    const S_Element=document.querySelector("#searchTxt")
+    const name = S_Element.value;
+    if (name === "") {
+     S_Element.classList.add("is-invalid");
+      return;
+    }
+    S_Element.classList.remove("is-invalid");
     const qrlink = "https://api.akuari.my.id/other/qrcode?text=";
     const linkqr = qrlink.concat(name);
 
     try {
       document.querySelector(".txtBox").innerHTML = `
+      <div class="card results-card  m-2 text-center">
+      <div class="card-header">
+        <h4>QR Code</h4>
+      </div>
+      <div class="card-body">
+      <h4>${name}</h4>
+
+      </div>
+      
       <div>
         <img
           src="${linkqr}"
-          class="rounded mx-auto d-block" alt="${name}" />
-      </div>`;
+          class="rounded mx-auto d-block " alt="${name}" />
+      </div>
+      </div>
+      `;
     }
     catch (error) {
       document.querySelector(".txtBox").innerHTML = `
